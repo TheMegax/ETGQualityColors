aa2bb953ef2f23017d5d3db33b73c3b82957d496
TheMegax <israkaya9@gmail.com>
2025-03-21 21:13:46 -0300

    Finishing up

--- Plugin.cs
+++ Plugin.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using BepInEx;
 using BepInEx.Bootstrap;
+using BepInEx.Configuration;
 using HarmonyLib;
 using RadialGunSelect;
 using UnityEngine;
@@ -21,6 +22,25 @@ public class Plugin : BaseUnityPlugin
     private const string VERSION = "1.0.0";
     private const string TEXT_COLOR = "#00FFFF";
 
+    private static ConfigEntry<Color> SQualityColor;
+    private static ConfigEntry<Color> AQualityColor;
+    private static ConfigEntry<Color> BQualityColor;
+    private static ConfigEntry<Color> CQualityColor;
+    private static ConfigEntry<Color> DQualityColor;
+    private static ConfigEntry<Color> SpecialQualityColor;
+    private static ConfigEntry<bool> UseColorsInWeaponWheelSelect;
+
+    public void Awake()
+    {
+        SQualityColor = Config.Bind("Colors", "QualityS", new Color(0.60f, 0.60f, 0.60f));
+        AQualityColor = Config.Bind("Colors", "QualityA", new Color(0.75f, 0.07f, 0.1f));
+        BQualityColor = Config.Bind("Colors", "QualityB", new Color(0.29f, 0.72f, 0.23f));
+        CQualityColor = Config.Bind("Colors", "QualityC", new Color(0.21f, 0.71f, 0.8f));
+        DQualityColor = Config.Bind("Colors", "QualityD", new Color(0.67f, 0.44f, 0.11f));
+        SpecialQualityColor = Config.Bind("Colors", "QualitySpecial",  new Color(0.5f, 0.2f, 1f));
+        UseColorsInWeaponWheelSelect = Config.Bind("Mod Compatibility", "UseColorsInWeaponWheelSelect", true);
+    }
+    
     public void Start()
     {
         ETGModMainBehaviour.WaitForGameManagerStart(GMStart);
@@ -41,12 +61,17 @@ public class Plugin : BaseUnityPlugin
         harmony.PatchAll(typeof(AmmonomiconPokedexEntry_LostFocus_Patch));
         harmony.PatchAll(typeof(AmmonomiconPageRenderer_DoRefreshData_Patch));
 
-        if (Chainloader.PluginInfos.ContainsKey("morphious86.etg.radialgunselect"))
+        if (UseColorsInWeaponWheelSelect.Value && Chainloader.PluginInfos.ContainsKey("morphious86.etg.radialgunselect"))
         {
             harmony.PatchAll(typeof(RadialSegment_SetHovered_Patch));
         }
+
+        ETGModConsole.Log("<color=#cecece>Quality</color> " +
+                          "<color=#BF121A>Colors</color> " +
+                          $"<color=#4AB83B>v{VERSION}</color> " +
+                          "<color=#36B5CC>started</color> " +
+                          "<color=#AB701C>successfully!</color>");
         
-        Log($"{NAME} v{VERSION} started successfully.", TEXT_COLOR);
     }
 
     public static void Log(string text, string color="#FFFFFF")
@@ -55,7 +80,8 @@ public class Plugin : BaseUnityPlugin
     }
     
     private static Color _outlineColor = Color.black;
-    
+    private static readonly int OutlineColor = Shader.PropertyToID("_OutlineColor");
+
     private static Color GetOutlineColor()
     {
         return _outlineColor;
@@ -65,13 +91,13 @@ public class Plugin : BaseUnityPlugin
     {
         _outlineColor = quality switch
         {
-            PickupObject.ItemQuality.S => new Color(0.60f, 0.60f, 0.60f),
-            PickupObject.ItemQuality.A => new Color(0.75f, 0.07f, 0.1f),
-            PickupObject.ItemQuality.B => new Color(0.29f, 0.72f, 0.23f),
-            PickupObject.ItemQuality.C => new Color(0.21f, 0.71f, 0.8f),
-            PickupObject.ItemQuality.D => new Color(0.67f, 0.44f, 0.11f),
-            PickupObject.ItemQuality.COMMON => new Color(0.67f, 0.44f, 0.11f),
-            PickupObject.ItemQuality.SPECIAL => new Color(0.5f, 0.2f, 1f),
+            PickupObject.ItemQuality.S => SQualityColor.Value,
+            PickupObject.ItemQuality.A => AQualityColor.Value,
+            PickupObject.ItemQuality.B => BQualityColor.Value,
+            PickupObject.ItemQuality.C => CQualityColor.Value,
+            PickupObject.ItemQuality.D => DQualityColor.Value,
+            PickupObject.ItemQuality.COMMON => DQualityColor.Value,
+            PickupObject.ItemQuality.SPECIAL => SpecialQualityColor.Value,
             _ => Color.black
         };
     }
@@ -97,7 +123,7 @@ public class Plugin : BaseUnityPlugin
     }
     
     // This is rather stupid, but despite what the docs say it seemingly only lets me do this one by one
-    
+    // I probably messed up somewhere yup
     [HarmonyPatch(typeof(Gun), nameof(Gun.OnExitRange))]
     public static class Gun_OnExitRange_Patch
     {
@@ -278,18 +304,21 @@ public class Plugin : BaseUnityPlugin
     public static class RadialSegment_SetHovered_Patch
     {
         // Sorry for replacing your code Morphious87 ^^"
-        private static void Postfix(
-            bool hovered, Color ___hoveredOutlineColor, Color ___unhoveredOutlineColor,
+        private static void Prefix(
+            ref bool __runOriginal, bool hovered, Color ___hoveredOutlineColor, Color ___unhoveredOutlineColor,
             Material ___material, tk2dSprite[] ___gunOutlineSprites, tk2dClippedSprite ___gunSprite, Gun ___originalGun)
         {
+            if (!__runOriginal)
+                return;
             var oCol = hovered ? ___hoveredOutlineColor : ___unhoveredOutlineColor;
-            ___material.SetColor("_OutlineColor", oCol);
+            ___material.SetColor(OutlineColor, oCol);
 
             if (___gunOutlineSprites == null) return;
             SetOutlineColor(___originalGun.quality);
             
             SpriteOutlineManager.RemoveOutlineFromSprite(___gunSprite);
             SpriteOutlineManager.AddOutlineToSprite(___gunSprite, _outlineColor, 0.1f);
+            __runOriginal = false;
         }
     }
 }
